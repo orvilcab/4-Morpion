15a100f1c57a54b918b45d4c4697cdef6628bb9a
orvilcab <126080124+orvilcab@users.noreply.github.com>
2023-04-03 17:11:39 +0200

    bug résolut

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const cells = document.querySelectorAll('[data-cell]'); //ont cache le DOM
 const gameStatus = document.getElementById('gameStatus');
 const endGameStatus = document.getElementById('endGameStatus');
-const playerOne = 'x'; const playerTwo = 'O';
+const playerOne = 'X'; const playerTwo = 'O';
 let playerTurn = playerOne;
 
 cells.forEach(cell => {
@@ -19,23 +19,22 @@ function updateGameStatus(status) {
    let statusText;
    
    switch (status){
-    case 'x':
-        statusText = "Au tour du joueur 2 (o)";
+    case 'X':
+        statusText = "Au tour du joueur 2 (O)";
         break;
-    case 'o':
-        statusText = "Au tour du joueur 1 (x)";
+    case 'O':
+        statusText = "Au tour du joueur 1 (X)";
         break;   
     case 'winsX':
-        statusText = "Le joueur 1 (x) a gagné!";
+        statusText = "Le joueur 1 (X) a gagné!";
         break; 
     case 'winsO':
-        statusText = "Le joueur 2 (o) a gagné!";
+        statusText = "Le joueur 2 (O) a gagné!";
         break;
     case 'draw':
         statusText = "Egalité! Personne ne gagne!";
         break;              
    }
-
    gameStatus.innerHTML = statusText;
    endGameStatus.innerHTML = statusText;
 }
